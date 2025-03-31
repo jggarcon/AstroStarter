ad37f2f07d0959a0d03e18544224d44ddf37b126
Mohammad Rashed <mo2002mohammad@gmail.com>
2025-03-31 19:11:02 -0300

    Added Components

--- components/MoreInfoButton.tsx
+++ components/MoreInfoButton.tsx
@@ -1,18 +1,8 @@
 import { useState } from "react";
 
 export default function MoreInfoButton() {
-  const [showInfo, setShowInfo] = useState(false);
 
+// add component implementation 
   return (
-    <div>
-      <button className="btn" onClick={() => setShowInfo(!showInfo)}>
-        {showInfo ? "Hide secret" : "Show Secret"}
-      </button>
-      {showInfo && (
-        <p>
-          Michael Jackson was a full-stack developer before he became a famous singer.
-        </p>
-      )}
-    </div>
   );
 }
